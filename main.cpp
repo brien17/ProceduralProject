2bd6e9434141aa37ee93fcd81bd6682c6d3034ae
brien17 <43622552+brien17@users.noreply.github.com>
2019-05-15 11:53:06 -0400

    test commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Production Line Tracker" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "1. Add Employee Account" << std::endl;
+    std::cout << "2. Add Music Player" << std::endl;
+    std::cout << "3. Add Movie Player" << std::endl;
+    std::cout << "4. Display Production Statistics" << std::endl;
+    std::cout << "5. Exit" << std::endl;
     return 0;
 }
