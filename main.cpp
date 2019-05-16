122233d27ca6ad62ac9f5f18f62d2f3d8df41c94
brien17 <43622552+brien17@users.noreply.github.com>
2019-05-15 22:25:38 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Production Line Tracker\n" << std::endl;
-    std::cout << "1. Add Employee Account" << std::endl;
-    std::cout << "2. Add Music Player" << std::endl;
-    std::cout << "3. Add Movie Player" << std::endl;
-    std::cout << "4. Display Production Statistics" << std::endl;
-    std::cout << "5. Exit" << std::endl;
+    cout << "Production Line Tracker\n" << endl;
+    cout << "1. Add Employee Account" << endl;
+    cout << "2. Add Music Player" << endl;
+    cout << "3. Add Movie Player" << endl;
+    cout << "4. Display Production Statistics" << endl;
+    cout << "5. Exit" << endl;
     return 0;
 }
