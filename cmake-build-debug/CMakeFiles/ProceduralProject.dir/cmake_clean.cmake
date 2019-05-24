1ce157f284e09238ebdc8c9f39fd1df39ae6ffce
brien17 <43622552+brien17@users.noreply.github.com>
2019-05-23 22:55:10 -0400

    Added functionality for produce items and add new product.

---
+++ cmake-build-debug/CMakeFiles/ProceduralProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProceduralProject.dir/main.cpp.obj"
+  "ProceduralProject.pdb"
+  "ProceduralProject.exe"
+  "ProceduralProject.exe.manifest"
+  "ProceduralProject.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProceduralProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
