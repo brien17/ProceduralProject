56187f710cb3f82d180ff0d8bc24773e46fd52a2
brien17 <43622552+brien17@users.noreply.github.com>
2019-06-05 12:40:24 -0400

    Added add employee function, login function, and encrypt password function

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
  * This file is production management software made for an imaginary company as an assignment for my COP 2001
  * course.
  * @author Cameron Brien
- * @bug  
+ * @bug
  */
 
 #include <iostream>
@@ -17,22 +17,17 @@
 
 //prototypes
 void show_menu();
-
 void get_input();
-
 void add_item();
-
 void produce_items();
-
 void production_statistics();
-
 void show_statistics_menu();
-
 void get_production_from_serial();
-
 void show_available_products_sorted();
-
+void add_employee_account();
+std::string log_in();
 std::string add_leading_zeros(int num);
+std::string encrypt_password(std::string password);
 
 /** The main method for my program that runs the other methods.
  *
@@ -55,7 +50,8 @@ void show_menu() {
     std::cout << "2. Add Employee Account" << std::endl;
     std::cout << "3. Add New Product" << std::endl;
     std::cout << "4. Display Production Statistics" << std::endl;
-    std::cout << "5. Exit" << std::endl;
+    std::cout << "5. Login" << std::endl;
+    std::cout << "6. Exit" << std::endl;
 }
 
 /**
@@ -65,10 +61,12 @@ void show_menu() {
 void get_input() {
     //declaring variable for user input
     int user_input;
+    //declaring a variable to hold user name
+    std::string current_user;
     //declaring boolean to track if the user is done with the program
     bool exit = false;
     //looping to get input from the user
-    while (exit == false) {
+    while (!exit) {
         //prompting the user
         std::cout << "Please select an option" << std::endl;
         //display a menu for the user showing the options
@@ -81,7 +79,7 @@ void get_input() {
                 produce_items();
                 break;
             case 2:
-                std::cout << "Add Employee Account Stub" << std::endl;
+                add_employee_account();
                 break;
             case 3:
                 add_item();
@@ -90,6 +88,9 @@ void get_input() {
                 production_statistics();
                 break;
             case 5:
+                current_user = log_in();
+                break;
+            case 6:
                 exit = true;
                 break;
             default:
@@ -116,32 +117,29 @@ void produce_items() {
     //ensuring good input
     unsigned int item_selected = 0;
     bool good_input = false;
-    //looping until good input is recieved
-    while (good_input == false) {
+    //looping until good input is received
+    while (!good_input) {
         //making sure the vector is clear
         items.clear();
+
         //creating placeholder for file data
         std::string line;
+
         //reading file
         std::ifstream catalog("catalog.csv");
 
         //checking if file is open
         if (catalog.is_open()) {
-            //declaring line counter
-            int line_counter = 0;
             //while there are more lines, takes the line and stores it as variable line
             while (getline(catalog, line)) {
                 //storing line in array
                 items.push_back(line);
-                //incrementing counter
-                line_counter++;
             }
         }
 
         //closing file
         catalog.close();
 
-        //entering letters makes it crash
         //prompting user
         std::cout << "Select an available item or add an item to be produced" << std::endl;
         //displaying available items
@@ -151,10 +149,12 @@ void produce_items() {
         //displaying the option to add new items
         std::cout << items.size() + 1 << ". " << "Add new item" << std::endl;
 
-        if(!(std::cin >> item_selected)) {
+        //checking if the user inputs the right type of data
+        if (!(std::cin >> item_selected)) {
             std::cin.clear();
             std::cin.ignore(10000, '\n');
         }
+
         //checking if the user selection is in the array, is add new item, or is bad input
         if (item_selected <= items.size() && item_selected > 0) {
             good_input = true;
@@ -185,33 +185,11 @@ void produce_items() {
     std::cin >> num_produced;
 
     //reading file
-    std::ifstream production_number_in;
-    production_number_in.open("production_number.txt");
-
-    //creating a string to store the data temporarily
-    std::string production_number_string;
-
-    //getting data as a string
-    getline(production_number_in, production_number_string);
-
-    //creating a variable to hold production number
-    int production_number = 1;
-
-    //closing file
-    production_number_in.close();
-
-    //creating a string stream of the production number
-    std::stringstream production_number_string_stream(production_number_string);
-
-    //storing that stream in the variable production number
-    production_number_string_stream >> production_number;
-
-    //reading file
     std::ifstream produced_in;
     produced_in.open("produced.csv");
 
     //creating a string to hold the first part of a serial number to match with others that have already been produced
-    std::string searchString = item_type_code;
+    std::string search_string = item_type_code;
 
     //creating placeholder for file data
     std::string line;
@@ -220,13 +198,13 @@ void produce_items() {
     std::string found = "not found";
 
     //creating a variable to hold number of the current line
-    unsigned int current_line = 0;
+    unsigned int production_number = 1;
 
-    //looping to find the last occurance of the serial number being searched for
+    //looping to find the last occurrence of the serial number being searched for
     while (getline(produced_in, line)) {
-        current_line++;
+        production_number++;
         //checking if the line contains the searched for serial number
-        if (line.find(searchString, 0) != std::string::npos) {
+        if (line.find(search_string, 0) != std::string::npos) {
             //setting the line matching the looked for serial number to found
             found = line;
         }
@@ -260,10 +238,6 @@ void produce_items() {
     std::ofstream produced_out;
     produced_out.open("produced.csv", std::ios_base::app);
 
-    //opening file
-    std::ofstream production_number_out;
-    production_number_out.open("production_number.txt");
-
     //looping to output production number and serial number and write to file
     for (int i = starting_number; i < ending_number; i++) {
         //outputting production number
@@ -274,17 +248,11 @@ void produce_items() {
         std::cout << " Serial Number: " << serialNumber << std::endl;
         //writing manufacturer, name, code, production number, and serial number file
         produced_out << manufacturer << "," << item_name << "," << item_type_code << "," << production_number
-                    << "," << serialNumber << "\n";
+                     << "," << serialNumber << "\n";
         //iterating production number
         ++production_number;
     }
 
-    //writing to file
-    production_number_out << production_number;
-
-    //closing file
-    produced_out.close();
-    production_number_out.close();
 }
 
 /**
@@ -295,7 +263,7 @@ void add_item() {
     // prompting the user for the manufacturer
     std::cout << "Enter the manufacturer" << std::endl;
     std::string manufacturer;
-    std::getline(std::cin,manufacturer);
+    std::getline(std::cin, manufacturer);
 
     //prompting the user for a product name
     std::cout << "Enter the product name" << std::endl;
@@ -309,7 +277,7 @@ void add_item() {
     //creating a boolean for the loop
     bool gotten_input = false;
     //looping to ensure the user enters good input
-    while (gotten_input == false) {
+    while (!gotten_input) {
         //printing menu
         std::cout << "Enter the item type\n";
         std::cout << "1. Audio\n" <<
@@ -357,8 +325,6 @@ void add_item() {
 
     //providing feedback to user
     std::cout << "Item has been saved" << std::endl;
-
-
 }
 
 /**
@@ -370,7 +336,7 @@ void production_statistics() {
     //declaring boolean to track if the user is done with the program
     bool exit = false;
     //looping to get input from the user
-    while (exit == false) {
+    while (!exit) {
         //prompting the user
         std::cout << "Welcome to the production statistics tab" << std::endl;
         std::cout << "Please select an option to continue" << std::endl;
@@ -475,8 +441,143 @@ void show_available_products_sorted() {
     //sorting
     sort(product_line_names.begin(), product_line_names.end());
     //printing items
-    for (std::string s : product_line_names)
+    for (std::string &s : product_line_names)
         std::cout << s << "\n";
+}
+
+/**
+ * This method allows the user to add a new user name and password, then it encrypts the password and writes the user
+ * name and password to a file.
+ */
+void add_employee_account() {
+    //clearing buffer
+    std::cin.ignore();
+    //giving instructions to the user
+    std::cout << "Please enter you first name and last name" << std::endl;
+    //creating first and last name
+    std::string first_name;
+    std::string last_name;
+    //getting input
+    std::cin >> first_name;
+    std::cin >> last_name;
+    //making first and last name lowercase
+    std::transform(first_name.begin(), first_name.end(), first_name.begin(), ::tolower);
+    std::transform(last_name.begin(), last_name.end(), last_name.begin(), ::tolower);
+    //creating user name
+    std::string user_name = first_name.at(0) + last_name;
+    //prompting user
+    std::cout << "Please enter a password (must contain a number, upper and lower case letters, and a symbol"
+              << std::endl;
+    bool good_password = false;
+    //creating a string for password
+    std::string password;
+    while (!good_password) {
+        std::cin.ignore();
+        //getting line from input
+        std::cin >> password;
+        //creating test variables
+        bool lower = false;
+        bool upper = false;
+        bool number = false;
+        bool symbol = false;
+        bool valid = false;
+        //looping to test each character of the password to see if it is legal
+        for (int i = 0; i < password.size(); i++){
+            if (islower(password[i])){
+                lower = true;
+            } else if (isupper(password[i])){
+                upper = true;
+            }else if (isdigit(password[i])){
+                number = true;
+            } else {
+                symbol = true;
+            }
+        }
+        //if there are no issues with the product
+        if (lower && upper && number && !symbol) {
+            valid = true;
+        }
+        //if the password is valid or not
+        if (valid) {
+            good_password = true;
+        } else {
+            std::cout << "Password not accepted" << std::endl;
+        }
+    }
+    //encrypting password
+    std::string encrypted_password = encrypt_password(password);
+
+    //creating file
+    std::ofstream employee_accounts_out;
+
+    //opening file
+    employee_accounts_out.open("employee_accounts.txt", std::ios_base::app);
+
+    //writing to file
+    employee_accounts_out << user_name << "," << encrypted_password << std::endl;
+
+    //closing file
+    employee_accounts_out.close();
+
+    //providing feedback to user
+    std::cout << "User name and password have been saved" << std::endl;
+}
+
+/**
+ * This method prompts the user for a username and password and if it is correct allows them to log in and returns their
+ * user name as a string.
+ * @return The user name of the user
+ */
+std::string log_in(){
+    //declaring a variable to hold user name
+    std::string user_name;
+    //declaring a variable to hold user name
+    std::string password;
+    //creating a variable to hold the string being searched for
+    std::string found = "not found";
+    //creating file
+    std::ifstream employee_accounts_in;
+    //opening file
+    employee_accounts_in.open("employee_accounts.txt");
+    //creating a variable to hold a line
+    std::string line;
+    //checking if file is open
+    if (employee_accounts_in.is_open()) {
+        //looping until a valid login
+        while (found == "not found") {
+            //resetting back to the beginning of the file
+            employee_accounts_in.clear();
+            employee_accounts_in.seekg(0, std::ios::beg);
+            //prompting user for user name
+            std::cout << "Enter your user name" << std::endl;
+            //storing input as user name
+            std::cin >> user_name;
+            //prompting user for password
+            std::cout << "Enter your password" << std::endl;
+            //storing input as user name
+            std::cin >> password;
+            //encrypting password
+            std::string encrypted_password = encrypt_password(password);
+            //creating a string to hold the information being searched for
+            std::string search_string = user_name.append("," + encrypted_password);
+            //looping to search for the password and user name
+            while (getline(employee_accounts_in, line)) {
+                //checking if the line contains the searched for serial number
+                if (line.find(search_string, 0) != std::string::npos) {
+                    //setting the line matching the looked for serial number to found
+                    found = line;
+                    std:: cout << "Login successful" << std::endl;
+                }
+            }
+            std::cout << "user name or password is incorrect" << std::endl;
+        }
+        //closing file
+        employee_accounts_in.close();
+        //returning user name
+        return user_name;
+    }else{
+        return "";
+    }
 }
 
 /**
@@ -500,4 +601,26 @@ std::string add_leading_zeros(int num) {
     std::string finalString = zerosString + numString;
     //returning the result
     return finalString;
+}
+
+/**
+ * This method encrypts the users password, passed as a string and returns the encrypted password as a string.
+ * @param password The users password
+ * @return The encrypted password
+ */
+std::string encrypt_password(std::string password){
+    int encrypt_number = 3;
+
+    if (password.length() == 1){
+        return password;
+    } else {
+        // get Ascii code of first letter by casting char to int
+        int code = (int)password[0];
+        // shift the Ascii code by adding the encrypt number
+        code += encrypt_number;
+        // convert the new Ascii code to a character by casting int to char
+        char code_char = (char)code;
+        // return the decrypted char + a recursive call to decrypt the next char
+        return (code_char + encrypt_password(password.substr(1, password.length() - 1)));
+    }
 }
