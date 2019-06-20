af88fc1d931f4a27a1c694e3d14dbef634995400
brien17 <43622552+brien17@users.noreply.github.com>
2019-06-20 16:55:47 -0400

    Added doc folder with doxygen documentation

---
+++ Doc/html/search/all_5.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['load_5fcatalog',['load_catalog',['../main_8cpp.html#a7892628793992ab0b5cec171fa6da420',1,'main.cpp']]],
+  ['load_5fproduction_5flog',['load_production_log',['../main_8cpp.html#a95c969f39c4e89b5734103376788e36d',1,'main.cpp']]],
+  ['load_5fstats',['load_stats',['../main_8cpp.html#a7bb055519ba7dd9d7309a310b18b3876',1,'main.cpp']]],
+  ['log_5fin',['log_in',['../main_8cpp.html#a578bfd32bab7d8966eed059cdb85da13',1,'main.cpp']]]
+];
---
+++ Doc/html/search/functions_5.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['print_5ffile',['print_file',['../main_8cpp.html#a57eba49e45be635861c7cb1b054d727b',1,'main.cpp']]],
+  ['print_5fproduced_5fby_5fitem_5fname',['print_produced_by_item_name',['../main_8cpp.html#acc8b889ab2842626acfdbc4d93ab2d94',1,'main.cpp']]],
+  ['print_5fproduced_5fby_5fmanufacturer',['print_produced_by_manufacturer',['../main_8cpp.html#a7c8bd439357b6efd6029ba158f2633e0',1,'main.cpp']]],
+  ['produce_5fitems',['produce_items',['../main_8cpp.html#a15624704042c85c9ed70c53b1bad8b89',1,'main.cpp']]],
+  ['production_5fstatistics',['production_statistics',['../main_8cpp.html#ae8c363fd795016a902a1e2cf1e3d4820',1,'main.cpp']]]
+];
